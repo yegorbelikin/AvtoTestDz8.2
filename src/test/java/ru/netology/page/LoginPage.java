e3123c950d6f5a0b25efbd105578b8f358d06e08
Yegor <yegor.belikin@gmail.com>
2025-04-02 19:18:26 +0500

    Correct deadline test

--- src/test/java/ru/netology/page/LoginPage.java
+++ src/test/java/ru/netology/page/LoginPage.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import ru.netology.data.DataHelper;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 
 
@@ -11,8 +12,8 @@ public class LoginPage {
 
     public final SelenideElement loginField = $("[data-test-id= 'login'] input");
     public final SelenideElement passwordField = $("[data-test-id= 'password'] input");
-    public final SelenideElement loginButton = $("[data-test-id= ]");
-    public final SelenideElement errorField = $("[data-test-id= ");
+    public final SelenideElement loginButton = $("[data-test-id= 'action-login']");
+    public final SelenideElement errorField = $("[data-test-id= 'error-notification'] .notification__content");
 
 
     public VerificationPage validLogin(DataHelper.AuthInfo info) {
@@ -23,9 +24,12 @@ public class LoginPage {
     public void login(DataHelper.AuthInfo info) {
         loginField.setValue(info.getLogin());
         passwordField.setValue(info.getPassword());
-        log
+        loginButton.click();
+    }
+
+
 
     public void errorNotification(String expectedText) {
-        errorField.shouldHave(Condition.exactText(expectedText)). shouldBe(Condition.visible);
+        errorField.shouldHave(exactText(expectedText)). shouldBe(Condition.visible);
     }
 }
